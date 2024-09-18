4465fb7518d4728f2b1daa6d68406d2f676d7fc1
tlaplana4 <tlaplana4@iespompeufabra.cat>
2024-09-18 10:36:19 +0200

    Update sketch.js

--- cara/sketch.js
+++ cara/sketch.js
@@ -1,9 +1,21 @@
-function setup() {
- createCanvas(600,400);
+function setup() 
+{
+  createCanvas(600,400);
 }
 
 function draw() {
- background(127);
- fill(227,192,139);
- ellipse(300,200,220,300);
+  background(127);
+  //cara
+  fill(227,192,139);
+  ellipse(300,200,250,300);
+  //Ull dret
+  fill(255);
+  ellipse(250,150,50,50);
+  fill(84,42,14);
+  ellipse(250,150,15,15);
+  //Ull esquerre
+  fill(255);
+  ellipse(350,150,50,50);
+  fill(84,42,14);
+  ellipse(350,150,15,15);
 }
