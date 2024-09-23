ec19c849578b273dbe83e676dff58659a7c698b5
tlaplana4 <tlaplana4@iespompeufabra.cat>
2024-09-23 13:38:45 +0200

    Update sketch.js

--- cara/sketch.js
+++ cara/sketch.js
@@ -9,6 +9,8 @@ function draw() {
   let vol= mic.getLevel();
   let vulls= map(vol,0,1,0,100);
   let vboca= map(vol,0,1,0,300);
+  console.log(vulls)
+  console.log(vol)
   background(127);
   //Cara
   fill(227,192,139);
@@ -17,12 +19,12 @@ function draw() {
   fill(255);
   ellipse(250,150,50,50);
   fill(84,42,14);
-  ellipse(250,150,15,15);
+  ellipse(250,150,10*sin(frameCount*0.1)+15,15);
   //Ull esquerre
   fill(255);
   ellipse(350,150,50,50);
   fill(84,42,14);
-  ellipse(350,150,15,15);
+  ellipse(350,150,10*cos(frameCount*0.1)+15,15);
   //Boca
   fill(200,15,15);
   arc(300,250,100,10+vboca,0,PI,CHORD);
