593cb92bb51edb2bf983e5d7cdb82d380980bbca
tlaplana4 <tlaplana4@iespompeufabra.cat>
2024-09-25 11:12:26 +0200

    Update sketch.js

--- ocells/sketch.js
+++ ocells/sketch.js
@@ -7,6 +7,9 @@ function setup() {
 }
 
 function draw() {
+  let ran=noise(0.01 * frameCount);
+  let ranX=map(ran, 0,1,0,50)
+  fill(200,50,10);
   background(220);
   vol=mic.getLevel();
   vpic=map(vol,0,1,0,100);
@@ -31,5 +34,23 @@ function draw() {
   //cos
   vertex(550,250);
   vertex(450,50);
+  endShape();
+  
+  fill(230);
+  beginShape();
+  vertex(400,150)
+  vertex(350,200)
+  vertex(400,250)
+  vertex(450,200)
+  vertex(400,150)
+  endShape();
+  
+  fill(78);
+  beginShape();
+  vertex(375+ranX,175)
+  vertex(350+ranX,200)
+  vertex(375+ranX,225)
+  vertex(400+ranX,200)
+  vertex(375+ranX,175)
   endShape();
 }
