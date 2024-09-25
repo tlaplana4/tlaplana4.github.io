7ec3723da007a58f8ed028ee6e53c55bb3be8dab
tlaplana4 <tlaplana4@iespompeufabra.cat>
2024-09-25 10:36:49 +0200

    Update sketch.js

--- cara/sketch.js
+++ cara/sketch.js
@@ -7,8 +7,6 @@ function setup()
 
 function draw() {
   let pseuRan= noise(frameCount*0.1);
-  let ranX = map(pseuRan,0,1,0,600);
-  let ranX = map(pseuRan,0,1,0,400);
   let cX =300;
   let cY =250;
   let vol= mic.getLevel();
