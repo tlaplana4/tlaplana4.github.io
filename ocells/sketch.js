e6597461906f768e3dbc290c840031ff4bac0d9b
tlaplana4 <tlaplana4@iespompeufabra.cat>
2024-09-25 10:34:32 +0200

    Update sketch.js

--- ocells/sketch.js
+++ ocells/sketch.js
@@ -1,7 +1,7 @@
 let mic;
 
 function setup() {
-  createCanvas(1000, 500);
+  createCanvas(1500, 1500);
   mic=new p5.AudioIn();
   mic.start();
 }
@@ -11,25 +11,25 @@ function draw() {
   vol=mic.getLevel();
   vpic=map(vol,0,1,0,10);
   beginShape();
-  vertex(60,10);
+  vertex(600,100);
   //pic
-  vertex(40,30);
-  vertex(20,40);
-  vertex(40,40);
-  vertex(20,40+vpic);
-  vertex(40,50);
+  vertex(400,300);
+  vertex(200,400);
+  vertex(400,400);
+  vertex(200,400+vpic);
+  vertex(400,500);
   //cos
-  vertex(60,90);
-  vertex(90,90);
-  vertex(120,40);
+  vertex(600,900);
+  vertex(900,900);
+  vertex(1200,400);
   //ploma
-  vertex(140,40);
-  vertex(135,35);
-  vertex(140,30);
-  vertex(130,30);
-  vertex(120,40);
+  vertex(1400,400);
+  vertex(1350,350);
+  vertex(1400,300);
+  vertex(1300,300);
+  vertex(1200,400);
   //cos
-  vertex(70,30);
-  vertex(60,10);
+  vertex(700,300);
+  vertex(600,100);
   endShape();
 }
